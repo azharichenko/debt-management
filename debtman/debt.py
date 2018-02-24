48877fd6187e102ca44dcebe815401aa9761a971
Hayden Stec <hrs24@pitt.edu>
2018-02-24 18:15:29 -0500

    Debugging and addition of minimum payment per month for loans

--- debtman/debt.py
+++ debtman/debt.py
@@ -8,7 +8,7 @@ COMPOUND_TYPE = {
 
 def debt_per_compound(balance, annual_interest, compound_rate):
     if compound_rate == "continuous":
-        return (balance * math.exp(annual_interest * (1 / 12))) - balance
+        return (balance * exp(annual_interest * (1 / 12))) - balance
     return balance * (annual_interest / COMPOUND_TYPE[compound_rate])
 
 def convert_dn_monthly(debt_per_comounding, compound_rate):
@@ -21,3 +21,6 @@ def sum_of_monthly_payments(lines_of_credit):
     for line in lines_of_credit:
         total += line["monthly_payment"]
     return total
+
+def loan_min_payment(balance, term, monthly_debt):
+    return (balance / term) + monthly_debt
