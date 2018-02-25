05e154849498b52bd417006e62cd610712bd660a
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2018-02-25 04:02:39 -0500

    :+1:

--- debtman/strategy.py
+++ debtman/strategy.py
@@ -1,9 +1,7 @@
 from debtman.debt import sum_of_monthly_payments, find_ratio, reach_28
 
-
 def _get_top_three(credit_lines):
-    data = [(line['interest_rate'], line) for line in credit_lines]
-    sorted_lines = sorted(data, key=lambda x: x[0], reverse=True)
+    sorted_lines = _sort_by_interest(credit_lines)
     top_three = sorted_lines[:3] if len(sorted_lines) >= 3 else sorted_lines
     return top_three
 
