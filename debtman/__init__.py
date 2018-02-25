2b1ad69bd4383313e489467bd852fc78e6425d24
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2018-02-25 01:22:09 -0500

    Savings current state of nonsense

--- debtman/__init__.py
+++ debtman/__init__.py
@@ -61,7 +61,7 @@ class CreditCardForm(FlaskForm):
     )
     compound_type = RadioField(
         "Select compound type: ",
-        choices=[('annual', "Annually"), ('biannual', "Biannually"), ('quarter', "Quarterly"), ('monthly', "Monthly")],
+        choices=[('annual', "Annually"), ('biannual', "Biannually"), ('quarter', "Quarterly"), ('monthly', "Monthly"), ('continous', 'Continously')],
         # Excluding compounded continously
         validators=[DataRequired()]
     )
@@ -84,7 +84,7 @@ class LoanForm(FlaskForm):
     )
     compound_type = RadioField(
         "Select compound type: ",
-        choices=[('annual', "Annually"),('biannual', "Biannually"), ('quarter' "Quarterly"), ('monthly', "Monthly")],  # Excluding compounded continously
+        choices=[('annual', "Annually"),('biannual', "Biannually"), ('quarter', "Quarterly"), ('monthly', "Monthly"), ('continous', 'Continously')],  # Excluding compounded continously
         validators=[DataRequired()]
     )
     term_length = IntegerField(
